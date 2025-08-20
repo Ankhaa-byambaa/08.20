b03113ac8025fa9e2ff4356738495390d7ead7a7
25LP5026 25LP5026 <25LP5026@G301-CC0207.local>
2025-08-20 12:56:46 +0800

    08.20-3

--- src/app/todo/page.js
+++ src/app/todo/page.js
@@ -1,24 +1,26 @@
 const Todo = () => {
   return (
     <>
-      <div className="bg-white w-[377px] h-[290px] rounded-[6px] flex gap-10 pt-6 pb-6 pl-4 pr-4 ">
+      <div className="bg-white w-[377px] h-[290px] rounded-[6px] flex flex-col gap-10 pt-6 pb-6 pl-4 pr-4 ">
         <div>
           <p className="text-black font-inter text-[20px] flex justify-center items-center">
             To-Do list
           </p>
           <div>
-            <input className=" bg-white"></input>
-            <button>add</button>
+            <input className="border-[1px] border-neutral-400 rounded-[6px] w-[280px] h-[40px] "></input>
+            <button>add</button>``
           </div>
-          <div>
-            <button>All</button>
-            <button>Acttive</button>
-            <button>Completed</button>
-          </div>
-          <p>No tasks yet. Add one above!</p>
+          <div className="flex gap-[6px] "></div>
+          <p className="text-[14px] text-gray-500 flex justify-center ">
+            No tasks yet. Add one above!
+          </p>
         </div>
-        <div>
-          Powered by <a href="#"> Pinecone Academy</a>
+        <div className="text-[12px] flex justify-center gap-[4px] ">
+          <p className=" text-gray-500">Powered by</p>
+          <a href="#" className="text-blue-400">
+            {" "}
+            Pinecone Academy
+          </a>
         </div>
       </div>
     </>
